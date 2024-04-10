a563d92da4605c7f631eb0e0d9329fc9f5de6f2d
csc340sp24 <M_GARCIAPAE@uncg.edu>
2024-04-10 15:36:59 -0400

    Implemented Edit functionality

--- CRUDApp/src/main/java/com/Assignment/CRUDApp/controllers/UserController.java
+++ CRUDApp/src/main/java/com/Assignment/CRUDApp/controllers/UserController.java
@@ -23,6 +23,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 /**
@@ -117,5 +118,92 @@ public class UserController {
         
         return "redirect:/users"; //after submit, redirect to users page
     } //stoppes on 40:45
+    
+    @GetMapping("/edit")
+    public String showEditPage(Model model, @RequestParam int id){
+        
+        try{
+            //read user details from db
+            User user = repo.findById(id).get(); 
+            //returns an obj of type optional
+            //.get converts it to User type
+            model.addAttribute("user", user);//make accessible to page
+            //Now me must create another userDto object, add it to the model
+            //add it to the page and it will used to bind to the form
+            
+            UserDto userDto = new UserDto();
+            userDto.setName(user.getName());
+            userDto.setAge(user.getAge());
+            userDto.setMajor(user.getMajor());
+            
+            model.addAttribute("userDto", userDto);
+            
+            
+        }catch(Exception ex){
+            System.out.println("Exception: " + ex.getMessage());
+            return "redirect: /users";
+        }
+        
+        return "users/editUser";
+    }
+    
+    
+    /*
+    * Model allows data to be sent to page
+    * userDto is for data of the submited form
+    * Binding result obj allows us to check if form data is valid
+    */
+    @PostMapping("/edit")
+    public String updateUser(Model model, @RequestParam int id,
+            @Valid @ModelAttribute UserDto userDto, BindingResult result){
+        
+        try {
+            
+            User user = repo.findById(id).get();
+            model.addAttribute("user", user);
+            
+            if (result.hasErrors()){
+                return "users/editUser";
+            }
+            
+            //checking for new image
+            if (!userDto.getImageFile().isEmpty()){
+                //delete old image
+                String uploadDir = "public/images";
+                Path oldImagePath = Paths.get(uploadDir + user.getImageFileName());
+                
+                try{
+                    Files.delete(oldImagePath);
+                } catch(Exception ex) {
+                    System.out.println("Exception: "  + ex.getMessage());
+                }
+                
+                //save new image
+                MultipartFile image = userDto.getImageFile();
+                String storageFileName = image.getOriginalFilename();
+                
+                try (InputStream inputStream = image.getInputStream()) {
+                    Files.copy(inputStream, Paths.get(uploadDir + storageFileName),
+                            StandardCopyOption.REPLACE_EXISTING);
+                }
+                
+                //update in DB
+                user.setImageFileName(storageFileName);      
+            } 
+            
+            //update the rest object in the DB
+            user.setName(userDto.getName());
+            user.setAge(userDto.getAge());
+            user.setMajor(userDto.getMajor());
+
+            repo.save(user);
+            
+        } catch (Exception ex){
+            System.out.println("Exception: " + ex.getMessage());
+        }
+        
+        return "redirect:/users";
+    }
+    
     
 }
